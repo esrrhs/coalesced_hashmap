de120819a7c03c816c8d6ee35d155af4b4b9bc14
esrrhs <esrrhs@163.com>
2024-06-13 17:36:53 +0800

    add test

--- test.cpp
+++ test.cpp
@@ -2,65 +2,68 @@
 
 using namespace coalesced_hashmap;
 
-int test() {
-    srand(time(NULL));
-    int N = 100;
-    CoalescedHashMap<std::string, int> map;
-    std::map<int, int> test_number;
-
-    for (int i = 0; i < N; i++) {
-        auto key = rand() % 10000000;
-        auto it = test_number.find(key);
-        if (it != test_number.end()) {
-            i--;
-            continue;
-        }
-        test_number[key] = i;
-    }
-
-    for (auto &i: test_number) {
-        map.Insert(std::to_string(i.first), i.second);
-    }
-
-    int value = 0;
-    for (auto &i: test_number) {
-        if (map.Find(std::to_string(i.first), value)) {
-            std::cout << "key: " << i.first << " value: " << value << std::endl;
-        } else {
-            std::cout << "key: " << i.first << " not found" << std::endl;
-            return -1;
-        }
-        if (value != i.second) {
-            std::cout << "key: " << i.first << " value: " << value << " not equal" << std::endl;
-            return -1;
-        }
-    }
-
-    std::cout << "size:" << map.Size() << std::endl;
-    std::cout << "capacity:" << map.Capacity() << std::endl;
-    std::cout << "main_position_size:" << map.MainPositionSize() << std::endl;
-    auto chain_status = map.ChainStatus();
-    for (auto &item: chain_status) {
-        std::cout << "chain length: " << item.first << " count: " << item.second << std::endl;
-    }
-
-    //std::cout << map.Dump() << std::endl;
-
-    for (auto it = map.Begin(); it != map.End(); ++it) {
-        std::cout << "key: " << it.GetKey() << " value: " << it.GetValue() << std::endl;
-    }
-
-    for (auto &i: test_number) {
-        if (!map.Erase(std::to_string(i.first))) {
-            std::cout << "erase key: " << i.first << " failed" << std::endl;
-            return -1;
-        }
-    }
-    return 0;
-}
-
 int64_t random_int64() {
     return (int64_t) rand() << 32 | rand();
+}
+
+int test() {
+    srand(time(NULL));
+    int N = 1000000;
+    CoalescedHashMap<std::string, int> map;
+
+    while (1) {
+        std::map<int64_t, int> test_number;
+
+        for (int i = 0; i < N; i++) {
+            auto key = random_int64();
+            test_number[key] = i;
+        }
+
+        std::cout << "test number size:" << test_number.size() << std::endl;
+
+        for (auto &i: test_number) {
+            map.Insert(std::to_string(i.first), i.second);
+        }
+
+        std::cout << "size:" << map.Size() << std::endl;
+
+        int value = 0;
+        for (auto &i: test_number) {
+            if (map.Find(std::to_string(i.first), value)) {
+//                std::cout << "key: " << i.first << " value: " << value << std::endl;
+            } else {
+                std::cout << "key: " << i.first << " not found" << std::endl;
+                return -1;
+            }
+            if (value != i.second) {
+                std::cout << "key: " << i.first << " value: " << value << " not equal" << std::endl;
+                return -1;
+            }
+        }
+
+        std::cout << "size:" << map.Size() << std::endl;
+        std::cout << "capacity:" << map.Capacity() << std::endl;
+        std::cout << "main_position_size:" << map.MainPositionSize() << std::endl;
+        auto chain_status = map.ChainStatus();
+        for (auto &item: chain_status) {
+            std::cout << "chain length: " << item.first << " count: " << item.second << std::endl;
+        }
+
+//        std::cout << map.Dump() << std::endl;
+
+//        for (auto it = map.Begin(); it != map.End(); ++it) {
+//            std::cout << "key: " << it.GetKey() << " value: " << it.GetValue() << std::endl;
+//        }
+
+        for (auto &i: test_number) {
+            if (!map.Erase(std::to_string(i.first))) {
+                std::cout << "erase key: " << i.first << " failed" << std::endl;
+                return -1;
+            }
+        }
+    }
+
+    return 0;
 }
 
 std::unordered_set<int64_t> gData;
